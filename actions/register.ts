0299e220a8b46ac2aea27fd32e73b00819c344d9
Aralaejung <aralaejung@gmail.com>
2024-08-29 15:51:17 +0700

    db prisma create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcryptjs';
+import { db } from '@/lib/db';
 
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -10,6 +13,21 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validateFields.success) {
     return { error: 'Invalid fields!' };
   }
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
   return { success: 'Email sent!' };
 };
