5c6abd5143aaabdfd68661d3a8baf3288b1fed40
Aralaejung <aralaejung@gmail.com>
2024-08-30 12:10:52 +0700

    OAuth

--- auth.ts
+++ auth.ts
@@ -13,33 +13,34 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  callbacks: {
-    async signIn({ user }) {
-      if (!user.id) {
-        return false; // Return false to stop the sign-in process if user ID is undefined
-      }
-
-      const existingUser = await getUserById(user.id);
-
-      if (!existingUser || !existingUser.emailVerified) {
-        return false;
-      }
-      return true;
+  pages: {
+    signIn: '/auth/login',
+    error: '/auth/error',
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
     },
+  },
+  callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
       return session;
     },
+
     async jwt({ token }) {
       if (!token.sub) return token;
 
       const existingUser = await getUserById(token.sub);
-
       if (!existingUser) return token;
 
       token.role = existingUser.role;
