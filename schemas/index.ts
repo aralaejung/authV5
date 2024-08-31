bb25d962ac4c1a178ef5e45dd8c7bd75c2411e60
Aralaejung <aralaejung@gmail.com>
2024-08-31 12:13:07 +0700

    reset password part2

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,7 @@
 import * as z from 'zod';
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, { message: 'Minimum 6 characters required' }),
+});
 
 export const ResetSchema = z.object({
   email: z.string().email({ message: 'Email is required' }),
