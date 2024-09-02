8174947ea08c073203c2b2de7cfb15f99e1e6c50
Aralaejung <aralaejung@gmail.com>
2024-09-02 10:48:55 +0700

    server & client

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,8 +2,8 @@ import NextAuth, { type DefaultSession } from 'next-auth';
 import { UserRole } from '@prisma/client';
 
 export type ExtendedUser = DefaultSession['user'] & {
-  id: string;
   role: UserRole;
+  isTwoFactorEnabled: boolean;
 };
 
 declare module 'next-auth' {
